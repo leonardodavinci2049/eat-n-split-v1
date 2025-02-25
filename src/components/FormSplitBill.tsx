5046cfc86178666756580186c472ba0d26b1fe90
Leonardo da Vinci <leonardodavinci2049@gmail.com>
2025-02-25 20:12:57 -0300

    feat: ajustes finais no layout responsivo do menu

--- src/components/FormSplitBill.tsx
+++ src/components/FormSplitBill.tsx
@@ -1,14 +1,16 @@
-import  { useState } from "react";
+import { useState } from "react";
 import Button from "./Button";
 import { FriendType } from "../data/friends";
-
 
 interface FormSplitBillProps {
   selectedFriend: FriendType;
   onSplitBill: (amount: number) => void;
 }
 
-const FormSplitBill: React.FC<FormSplitBillProps> = ({ selectedFriend, onSplitBill }) => {
+const FormSplitBill: React.FC<FormSplitBillProps> = ({
+  selectedFriend,
+  onSplitBill,
+}) => {
   const [bill, setBill] = useState<number | "">("");
   const [paidByUser, setPaidByUser] = useState<number | "">("");
   const paidByFriend = bill ? Number(bill) - Number(paidByUser) : 0;
@@ -22,36 +24,56 @@ const FormSplitBill: React.FC<FormSplitBillProps> = ({ selectedFriend, onSplitBi
   }
 
   return (
-    <form className="form-split-bill" onSubmit={handleSubmit}>
-      <h2>Split a bill with {selectedFriend.name}</h2>
-      <label htmlFor="bill">💰 Bill value</label>
+    <form
+      className="grid grid-cols-[1fr_12rem] px-16 py-12 bg-lightest"
+      onSubmit={handleSubmit}
+    >
+      <h2 className="col-span-full text-[2.2rem] uppercase tracking-tight mb-6 font-bold">
+        Split a bill with {selectedFriend.name}
+      </h2>
+      <label htmlFor="bill" className="text-2xl font-semibold">
+        💰 Bill value
+      </label>
       <input
         id="bill"
         type="text"
         value={bill}
         onChange={(e) => setBill(Number(e.target.value))}
+        className="font-sans text-inherit text-[1.5rem] p-3 text-center border border-gray-200 rounded transition-all duration-300 focus:outline-none focus:border-gray-600"
       />
-      <label htmlFor="">🧍‍♀️ Your expense</label>
+      <label htmlFor="" className="text-2xl font-semibold">
+        🧍‍♀️ Your expense
+      </label>
       <input
         type="text"
         value={paidByUser}
         onChange={(e) =>
           setPaidByUser(
-            Number(e.target.value) > Number(bill) ? paidByUser : Number(e.target.value)
+            Number(e.target.value) > Number(bill)
+              ? paidByUser
+              : Number(e.target.value)
           )
         }
+        className="font-sans text-inherit text-[1.5rem] p-3 text-center border border-gray-200 rounded transition-all duration-300 focus:outline-none focus:border-gray-600"
       />
-      <label>👫 {selectedFriend.name}'s expense</label>
-      <input type="text" disabled value={paidByFriend} />
-      <label>🤑 Who is paying the bill</label>
+      <label className="text-2xl font-semibold">👫 {selectedFriend.name}'s expense</label>
+      <input
+        type="text"
+        disabled
+        value={paidByFriend}
+        className="font-sans text-inherit text-[1.5rem] p-3 text-center border border-gray-200 rounded transition-all duration-300 focus:outline-none focus:border-gray-600"
+      />
+      <label className="text-2xl font-semibold">🤑 Who is paying the bill</label>
       <select
         value={whoIsPaying}
         onChange={(e) => setWhoIsPaying(e.target.value)}
+        className="font-sans text-inherit text-[1.5rem] p-3 mb-4  text-center border border-gray-200 rounded transition-all duration-300 focus:outline-none focus:border-gray-600"
       >
         <option value="user">You</option>
         <option value="friend">{selectedFriend.name}</option>
       </select>
-      <Button>Split bill</Button>
+
+      <Button styleAdd="">Split bill</Button>
     </form>
   );
 };
--- src/components/Friend.tsx
+++ src/components/Friend.tsx
@@ -5,31 +5,38 @@ interface FriendProps {
   friend: FriendType;
   selectedFriend: FriendType | null;
   onSelection: (friend: FriendType) => void;
-};
-
-
+}
 
 const Friend = ({ friend, selectedFriend, onSelection }: FriendProps) => {
-
-
-
   const isSelected = selectedFriend?.id === friend.id;
   return (
-    <li className={isSelected ? "selected" : ""}>
-      <img src={friend.image} alt={friend.name} />
-      <h3>{friend.name}</h3>
+    <li
+      className={`grid grid-cols-[4.8rem_1fr_auto] items-center gap-x-6 p-5 rounded-lg transition duration-500 ${
+        isSelected ? "selected" : ""
+      }`}
+    >
+      <img
+        src={friend.image}
+        alt={friend.name}
+        className="rounded-full w-full row-span-2"
+      />
+      <h3 className="row-start-1 col-start-2">{friend.name}</h3>
 
       {friend.balance < 0 && (
-        <p className="red">
+        <p className="row-start-2 col-start-2 text-red-500">
           You owe {friend.name} {Math.abs(friend.balance)}€
         </p>
       )}
       {friend.balance > 0 && (
-        <p className="green">
+        <p className="row-start-2 col-start-2 text-green-500">
           {friend.name} owes you {Math.abs(friend.balance)}€
         </p>
       )}
-      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+      {friend.balance === 0 && (
+        <p className="row-start-2 col-start-2">
+          You and {friend.name} are even
+        </p>
+      )}
 
       <Button onClick={() => onSelection(friend)}>
         {isSelected ? "Close" : "Select"}
